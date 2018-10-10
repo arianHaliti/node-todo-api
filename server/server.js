06b503c57ac4062d4e7c1fe0ac584f480b88f8c3
Arian Haliti <arian93haliti@gmail.com>
2018-10-10 21:50:26 +0200

    Added route for todos and modules

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var { Todo } = require("../modules/todos");
+var { mongoose } = require("../db/db.js");
+var { User } = require("../modules/users");
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post("/todos", (req, res) => {
+  var newTodo = new Todo({
+    text: req.body.text
+  });
+
+  newTodo.save().then(
+    doc => {
+      console.log(doc);
+      res.status(200).send(doc);
+    },
+    e => {
+      console.log(e);
+      res.status(400).send(e);
+    }
+  );
+});
+
+app.listen(3000, () => {
+  console.log("Lisening to port 3000");
+});
