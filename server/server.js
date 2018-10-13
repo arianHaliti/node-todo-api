034316c903036c8a7481d6845619f4b0dda74218
Arian Haliti <arian93haliti@gmail.com>
2018-10-14 00:29:49 +0200

    Add GET todos ID

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const { ObjectId } = require("mongodb");
 var { Todo } = require("../models/todos");
 var { mongoose } = require("../db/db.js");
 var { User } = require("../models/users");
@@ -37,6 +37,23 @@ app.post("/users", (req, res) => {
     e => {
       console.log(e);
       res.status(400).send(e);
+    }
+  );
+});
+// GET Todos/:id route
+
+app.get("/todos/:id", (req, res) => {
+  let id = req.params.id;
+  if (!ObjectId.isValid(id)) return res.status(404).send();
+  Todo.findById({
+    _id: id
+  }).then(
+    todo => {
+      if (todo == null) return res.status(404).send();
+      res.send({ todo });
+    },
+    e => {
+      res.status(404).send();
     }
   );
 });
